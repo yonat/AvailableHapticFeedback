f53351d6b4daf26c2774bd99a1cb7c43b15d9e17
Yonat Sharon <yonat@ootips.org>
2019-08-21 22:57:25 +0300

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "AvailableHapticFeedback",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library( name: "AvailableHapticFeedback", targets: ["AvailableHapticFeedback"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "AvailableHapticFeedback", dependencies: [], path: "Sources")
+    ],
+    swiftLanguageVersions: [.v5]
+)
